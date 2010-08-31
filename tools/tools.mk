149facc40856d1eeb59df0b084bd774e8edc5f19
Neale Pickett <neale@woozle.org>
2010-08-30 23:24:47 -0500

    Add bubble babble digest tool

--- tools/tools.mk
+++ tools/tools.mk
@@ -1,6 +1,10 @@
 PLAIN += tools
-COPY += tools/zar tools/zdec tools/zpex
+COPY += tools/zar tools/zdec tools/zpex tools/bubblebabble.c
 
 $(DESTDIR)/tools/%: /home/neale/bin/%
+	@mkdir -p $(@D)
+	cp $< $@
+
+$(DESTDIR)/tools/%: /home/neale/src/c/%
 	@mkdir -p $(@D)
 	cp $< $@
