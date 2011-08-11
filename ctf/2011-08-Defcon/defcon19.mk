4a63dbe944091b186ea468f0ade91fb1125ad084
Neale Pickett <neale@woozle.org>
2011-08-11 16:15:33 -0500

    Mention EFF donations at Defcon 19

---
+++ ctf/2011-08-Defcon/defcon19.mk
@@ -0,0 +1,2 @@
+COPY += ctf/2011-08-Defcon/p2inv.png
+COPY += ctf/2011-08-Defcon/scoreboard.html
--- ctf/ctf.mk
+++ ctf/ctf.mk
@@ -3,5 +3,4 @@ COPY += ctf/register.cgi
 
 PLAIN += ctf/*/
 
-COPY += ctf/2011-08-Defcon/p2inv.png 
-COPY += ctf/2011-08-Defcon/scoreboard.html
+-include ctf/*/*.mk
