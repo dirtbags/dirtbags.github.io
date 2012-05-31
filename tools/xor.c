f51337b574d595fbfb445efd66d1c2b031f08829
Neale Pickett <neale@lanl.gov>
2012-05-31 15:28:06 -0600

    Add copying information

--- tools/xor.c
+++ tools/xor.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int
@@ -16,6 +17,8 @@ main(int argc, char *argv[])
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s [-x] m1 [m2 ...]\n", argv[0]);
+        fprintf(stderr, "\n");
+        fprintf(stderr, "The -x is for hex.  Otherwise, use decimal (or 0xHEX or 0OCT).\n");
         return 1;
     }
 
