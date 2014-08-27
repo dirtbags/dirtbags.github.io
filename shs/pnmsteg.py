d6f73994bc32d2887f022f0fcc3cae8ba4f49d59
Neale Pickett <neale@woozle.org>
2014-08-27 18:54:50 +0000

    Fix the duck

--- shs/pnmsteg.py
+++ shs/pnmsteg.py
@@ -10,19 +10,20 @@ sys.stdout.buffer.write(sys.stdin.buffer.readline())
 sys.stdout.buffer.write(sys.stdin.buffer.readline())
 
 while True:
-    medium = sys.stdin.buffer.read(8)
-    if len(medium) < 8:
-        sys.stdout.buffer.write(medium)
-        break
+	medium = sys.stdin.buffer.read(8)
+	if len(medium) < 8:
+		sys.stdout.buffer.write(medium)
+		break
 
-    message = plaintext.read(1)
-    if not message:
-        sys.stdout.buffer.write(medium)
-        continue
+	message = plaintext.read(1)
+	if not message:
+		sys.stdout.buffer.write(medium)
+		continue
 
-    for i in range(8):
-        j = 8 - i
-        bit = (ord(message) & (1 << j)) >> j
-        mask = 0xfe | bit
-        out = bytes(medium[i] & mask)
-        sys.stdout.buffer.write(out)
+	for i in range(8):
+		j = 7 - i
+		bit = (ord(message) & (1 << j)) >> j
+		mask = 0xfe | bit
+		out = bytes([medium[i] & mask])
+		sys.stdout.buffer.write(out)
+
